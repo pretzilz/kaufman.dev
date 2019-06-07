8b9c40896a89a0556e3a7d883c73e2c5a52fabf2
Kurt Kaufman <kurt.m.kaufman@gmail.com>
2019-06-06 21:40:42 -0600

    Bit more stubs for now

--- scripts/nopebbles.js
+++ scripts/nopebbles.js
@@ -1,12 +1,15 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoicHJldHppbHoiLCJhIjoiY2p3bGdvdzFmMDNmYTRhbWN0ZGk5MGVxeSJ9.M5lPXsMlvwOa_ablXgwLyQ';
 
 var NoPebbles = {
+    Map: {},
+
     Init: function() {
         this.InitializeMap();
+        this.InitializeButtons();
     },
 
     InitializeMap: function() {
-        var map = new mapboxgl.Map({
+        this.map = new mapboxgl.Map({
             container: 'map',
             style: 'mapbox://styles/mapbox/streets-v11',
             center: [-96, 37.8],
@@ -14,18 +17,34 @@ var NoPebbles = {
         
         });
         
-        map.addControl(new mapboxgl.GeolocateControl({
+        this.map.addControl(new mapboxgl.GeolocateControl({
             positionOptions: {
                 enableHighAccuracy: true
             },
             trackUserLocation: false
-        }));
-        
-        
-        map.on('click', function (e) {
+        }));         
+    },
+
+    InitializeButtons: function() {
+        document.getElementById('enterTrip').addEventListener('click', this.StartTrip);
+        document.getElementById('saveTrip').addEventListener('click', this.SaveTrip);
+        document.getElementById('cancelTrip').addEventListener('click', this.CancelTrip);
+    },
+
+    StartTrip: function() {
+        // attach a click to the map to grab the latitude and longitude, and place a marker there
+        NoPebbles.map.on('click', function (e) {
             // e.lngLat is the longitude, latitude geographical position of the event
             console.log(e.lngLat);
-        });        
+        }); 
+    },
+
+    SaveTrip: function() { 
+
+    },
+
+    CancelTrip: function() {
+
     }
 
 }
