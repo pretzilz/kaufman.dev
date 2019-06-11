785a8fa953d1a80b686bb771598c2070ecba0f6e
Kurt Kaufman <kurt.m.kaufman@gmail.com>
2019-06-10 18:07:46 -0600

    Attempting to add points on click.

--- scripts/nopebbles.js
+++ scripts/nopebbles.js
@@ -2,14 +2,14 @@ mapboxgl.accessToken = 'pk.eyJ1IjoicHJldHppbHoiLCJhIjoiY2p3bGdvdzFmMDNmYTRhbWN0Z
 
 var NoPebbles = {
     Map: {},
-
+    PointData: turf.featureCollection([]),
     Init: function() {
         this.InitializeMap();
         this.InitializeButtons();
     },
 
     InitializeMap: function() {
-        this.map = new mapboxgl.Map({
+        NoPebbles.Map = new mapboxgl.Map({
             container: 'map',
             style: 'mapbox://styles/mapbox/streets-v11',
             center: [-96, 37.8],
@@ -17,12 +17,39 @@ var NoPebbles = {
         
         });
         
-        this.map.addControl(new mapboxgl.GeolocateControl({
+        NoPebbles.Map.addControl(new mapboxgl.GeolocateControl({
             positionOptions: {
                 enableHighAccuracy: true
             },
             trackUserLocation: false
-        }));         
+        }));  
+
+        NoPebbles.Map.on('load', () => {
+            NoPebbles.Map.addSource("points", {
+                "type": "geojson",
+                "data": {
+                    "type": "FeatureCollection",
+                    "features": []
+                }
+            });    
+
+            NoPebbles.Map.addLayer({
+                'id': 'point',
+                'type': 'circle',
+                'source': 'points',
+                'paint': {
+                    // make circles larger as the user zooms from z12 to z22
+                    'circle-radius': {
+                        'base': 8,
+                        'stops': [[12, 2], [22, 180]]
+                    },
+                    'circle-color': '#00b7bf',
+                    'circle-radius': 8,
+                    'circle-stroke-width': 1,
+                    'circle-stroke-color': '#333',
+                }
+            });   
+        })
     },
 
     InitializeButtons: function() {
@@ -33,9 +60,19 @@ var NoPebbles = {
 
     StartTrip: function() {
         // attach a click to the map to grab the latitude and longitude, and place a marker there
-        NoPebbles.map.on('click', function (e) {
+        NoPebbles.Map.on('click', function (e) {
             // e.lngLat is the longitude, latitude geographical position of the event
             console.log(e.lngLat);
+            
+            NoPebbles.PointData.features.push({
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": e.lngLat
+                }
+            });
+            
+            NoPebbles.Map.getSource('points').setData(NoPebbles.PointData);   
         }); 
     },
 
